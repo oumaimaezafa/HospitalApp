7ec30b624d1633d83f38eccc7051fb32cf1b4185
oumaimaezafa <oumayma.ezafa@gmail.com>
2024-03-16 15:26:40 +0100

    Spring sceurity part 3

--- src/main/java/ma/enset/apphospital/security/SecurityConfig.java
+++ src/main/java/ma/enset/apphospital/security/SecurityConfig.java
@@ -3,9 +3,12 @@ package ma.enset.apphospital.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -13,6 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -20,18 +24,26 @@ public class SecurityConfig {
     //c'est pour chercher les utilisateurs , preciser en memoire les utilisateurs quiont le droit de faire aurhetification
    //IL FAUT UTILISER LE PASS ENCODER
 
+
+
     //Configure authentication mechanism
     @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
         return  new InMemoryUserDetailsManager(
                 User.withUsername("user1").password(passwordEncoder.encode("1234")).roles("USER").build(),
                 User.withUsername("user2").password(passwordEncoder.encode("1234")).roles("USER").build(),
-                User.withUsername("user3").password(passwordEncoder.encode("1234")).roles("USER","ADMIN").build()
+                User.withUsername("admin").password(passwordEncoder.encode("1234")).roles("USER","ADMIN").build()
         );
     }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.formLogin();
+        http.formLogin().loginPage("/login").permitAll();
+        http.rememberMe();
+        http.authorizeRequests().requestMatchers("/webjars/**").permitAll();
+        //http.authorizeRequests().requestMatchers("/user/**").hasRole("USER");
+        //http.authorizeRequests().requestMatchers("/admin/**").hasRole("ADMIN");
+        // POUR AFFICHER UN ERREUR A UN UTILISATEUR QUE N'A PAS LE DROIT
+        http.exceptionHandling().accessDeniedPage("/notAuthorized");
         http.authorizeRequests()
                 .anyRequest().authenticated();
         return http.build();
--- src/main/java/ma/enset/apphospital/web/PatientController.java
+++ src/main/java/ma/enset/apphospital/web/PatientController.java
@@ -8,6 +8,7 @@ import ma.enset.apphospital.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -26,7 +27,7 @@ public class PatientController {
 
     //je creer une methode qui retourne un String permet de retourner un vue
 
-    @GetMapping("/index")
+    @GetMapping("/user/index")
     public String index(Model model, @RequestParam(name = "page",defaultValue = "0") int page,
                         @RequestParam(name = "size",defaultValue = "7") int size,
                         @RequestParam(name = "keyword",defaultValue = "") String kw){
@@ -40,31 +41,33 @@ public class PatientController {
 
          return "patients";
     }
-
-    @GetMapping("/delete")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/admin/delete")
     public String delete(Long id, @RequestParam(value = "keyword" , defaultValue = "") String keyword, @RequestParam (value = "page", defaultValue ="0") int page){
         patientRepository.deleteById(id);
-        return "redirect:/index?page="+page+"&keyword="+keyword;
+        return "redirect:/user/index?page="+page+"&keyword="+keyword;
     }
     @GetMapping("/")
     public String home(){
-        return "redirect:/index";
+        return "redirect:/user/index";
     }
-
-    @GetMapping("/formPatients")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/admin/formPatients")
     public String formPatient(Model model){
         model.addAttribute("patient",new Patient());
         return "formPatients";
     }
 
-    @PostMapping(path = "/save")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping(path = "/admin/save")
     public String save(Model model, @Valid Patient patient, BindingResult bindingResult,@RequestParam(defaultValue = "0") int page,@RequestParam(defaultValue = "") String keyword){
         if (bindingResult.hasErrors())return  "formPatients";
         patientRepository.save(patient);
-        return "redirect:/index?page="+page+"&keyword="+keyword;
+        return "redirect:/user/index?page="+page+"&keyword="+keyword;
     }
 
-    @GetMapping("/editPatient")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/admin/editPatient")
     public String editPatient(Model model,Long id,String keyword,int page ){
         Patient patient=patientRepository.findById(id).orElse(null);
         if(patient ==null) throw  new RuntimeException("Patient introuvable !!");
